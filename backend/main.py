51f6276f8985a901bb461f84596ad8b9c1f424d5
Qiao Zhang <zqthedesigner@gmail.com>
2022-03-03 00:40:10 +0800

    test

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST'])
+@app.route('/',  methods=['GET', 'POST'])
 def query_example():
     request_data = request.get_json()
     print(request_data)
